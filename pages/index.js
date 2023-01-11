5a52f32f6bfd673b547f6ef0901ace2852005c7c
Lucas Santos <lsantos@southend.com.ar>
2023-01-11 15:41:32 -0300

    update todo list

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ export default function FirstPost() {
         <li>OKR: Increase process performance for recurrent/quick/spot purchases</li>
         <li>NOW System receives purchase requisition items from external source. Allow REST API to push data</li>
         <li>NOW Selection screen to create manually a Market quote directly</li>
+        <li>NEXT allow buyer to cancel market sourcing</li>
         <li>NEXT Allow automation rules</li>
         <li>NEXT Process outcome from Ariba using WebServices</li>
         <li>LATER Create detailed view of the quotation process from Ariba, allowing the buyer to edit and changes to reflect Ariba</li>
