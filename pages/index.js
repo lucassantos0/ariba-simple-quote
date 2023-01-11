90fd255b10f9808d49c84de971a3c437f1001317
Lucas Santos <lsantos@southend.com.ar>
2023-01-11 15:23:20 -0300

    added OKR list as a friendly reminder :)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return <ul>
+        <li>OKR: Increase process performance for recurrent/quick/spot purchases</li>
+        <li>NOW System receives purchase requisition items from external source. Allow REST API to push data</li>
+        <li>NOW Selection screen to create manually a Market quote directly</li>
+        <li>NEXT Allow automation rules</li>
+        <li>NEXT Process outcome from Ariba using WebServices</li>
+        <li>LATER Create detailed view of the quotation process from Ariba, allowing the buyer to edit and changes to reflect Ariba</li>
+        <li>LATER Allow automatic award based on rules</li>
+    </ul>;
 }
