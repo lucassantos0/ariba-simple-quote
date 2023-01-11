0bc188d4430981360cef1dd9678f11715c08f42d
Lucas Santos <lsantos@southend.com.ar>
2023-01-11 18:12:23 -0300

    adding components dependency

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import Example from '../components/purchase-req-tableExample'
+
 export default function FirstPost() {
-    return <ul>
+    return (<div><ul>
         <li>OKR: Increase process performance for recurrent/quick/spot purchases</li>
         <li>NOW System receives purchase requisition items from external source. Allow REST API to push data</li>
         <li>NOW Selection screen to create manually a Market quote directly</li>
@@ -8,5 +10,8 @@ export default function FirstPost() {
         <li>NEXT Process outcome from Ariba using WebServices</li>
         <li>LATER Create detailed view of the quotation process from Ariba, allowing the buyer to edit and changes to reflect Ariba</li>
         <li>LATER Allow automatic award based on rules</li>
-    </ul>;
+    </ul>
+        <br />
+        <Example /></div>
+    );
 }
